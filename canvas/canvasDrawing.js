c389bad58a911a7b87bd27c721da10b5aae8864c
Dasha <darja.portnova@gmail.com>
2024-03-19 10:34:01 +0200

    path to canvas, and just canvas

---
+++ canvas/canvasDrawing.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById("canvas");
+
+let context = canvas.getContext("2d");
+
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.height = window_height;
+
+canvas.style.background = "#ff8";
+
+context.fillRect(0, 0, 100, 100);
